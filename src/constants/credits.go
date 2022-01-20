000cfe70236b4db6bca32e15fc45ec955f68eff0
august <bringmetheaugust@gmail.com>
2022-01-20 23:47:46 +0200

    new folder struct

--- src/constants/credits.go
+++
@@ -1,4 +0,0 @@
-package constants
-
-const Version = "0.0.2"
-const Author = "August Luzanovsky, 2021"
--- src/keys/help.go
+++
@@ -1,7 +0,0 @@
-package keys
-
-import "fmt"
-
-func Help() {
-	fmt.Println("Andy's helper🐼\n\nOptions:\n  --version, -v       application version")
-}
