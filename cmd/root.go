f016f15aa0b33c01b2f325cb7363c081a245c545
bringmetheaugust <bringmetheaugust@gmail.com>
2024-01-07 13:19:51 +0200

    disable auto version

--- cmd/root.go
+++ cmd/root.go
@@ -1,38 +1,35 @@
 package commands
 
 import (
-	"os"
-	"path/filepath"
-
 	"github.com/spf13/cobra"
-	"gopkg.in/yaml.v3"
 )
 
-type Meta struct {
-	Version string `yaml:"version"`
-}
+// type Meta struct {
+// 	Version string `yaml:"version"`
+// }
 
-func getVersion() string {
-	var meta Meta
+// func getVersion() string {
+// 	var meta Meta
 
-	metaFile, _ := filepath.Abs("./configs/meta.yaml")
-	yamlFile, yamlFileErr := os.ReadFile(metaFile)
+// 	metaFile, _ := filepath.Abs("./configs/meta.yaml")
+// 	yamlFile, yamlFileErr := os.ReadFile(metaFile)
 
-	if yamlFileErr != nil {
-		panic(yamlFileErr)
-	}
+// 	if yamlFileErr != nil {
+// 		panic(yamlFileErr)
+// 	}
 
-	if err := yaml.Unmarshal(yamlFile, &meta); err != nil {
-		panic(err)
-	}
+// 	if err := yaml.Unmarshal(yamlFile, &meta); err != nil {
+// 		panic(err)
+// 	}
 
-	return meta.Version
-}
+// 	return meta.Version
+// }
 
 var RootCmd = &cobra.Command{
 	Short: "Hi! I'm Andy🐼!",
 	CompletionOptions: cobra.CompletionOptions{
 		DisableDefaultCmd: true,
 	},
-	Version: getVersion(),
+	// Version: getVersion(),
+	Version: "0.0.2",
 }
